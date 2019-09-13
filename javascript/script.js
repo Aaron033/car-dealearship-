4a1072ba02f820a766014e30b02b0fac5536eec9
Aaron <aaron.botello.h@gmail.com>
2019-09-12 19:40:24 -0500

    code commented out

--- javascript/script.js
+++ javascript/script.js
@@ -14,19 +14,24 @@ function carousel() {
 }
 
 
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-    this.classList.toggle( "active" );
-  });
+// document.querySelector( "#nav-toggle" )
+//   .addEventListener( "click", function() {
+//     this.classList.toggle( "active" );
+//   });
 
-document.querySelector("#nav-toggle").addEventListener("click", menuNav);
+// document.querySelector("#nav-toggle").addEventListener("click", menuNav);
 
 // function menuNav() {
 //   document.querySelector(".tabs").style.height = "200px";
   
 // }
-
+let content = document.querySelector('.tabs'); 
 let navBar = document.querySelector("#nav-toggle");
+
+navBar.onclick = function(){
+
+  if(content.className == )
+}
 
 // onclick = function() {
 //   if(navBar.className == "open") {
