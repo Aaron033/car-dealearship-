7688977228fb3da563a73eb246af0512b6749af4
Aaron <aaron.botello.h@gmail.com>
2019-09-19 22:49:54 -0500

    subcode js added

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,15 @@
-// var myIndex = 0;
-// carousel();
+var myIndex = 0;
+carousel();
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {myIndex = 1}    
-//   x[myIndex-1].style.display = "block";  
-//   setTimeout(carousel, 4000); 
-// }
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000); 
+}
 
-
-(function() {
-  let burger = document.querySelector('.burger'); 
-  let nav = document.querySelector('#' +burger.dataset.target); 
-
-  burger.addEventListener('click', function(){
-    burger.classList.toggle('is-active'); 
-    nav.classList.toggle('is-active');
-  });
-})();
