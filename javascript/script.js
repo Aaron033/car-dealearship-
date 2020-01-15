a016e3cbae8a9abb50bfbfae53d9b16592265da8
Aaron <aaron.botello.h@gmail.com>
2020-01-14 20:52:25 -0600

    cms deleted

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ carousel();
 
 function carousel() {
   var i;
+  //Checking all of slides 
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
